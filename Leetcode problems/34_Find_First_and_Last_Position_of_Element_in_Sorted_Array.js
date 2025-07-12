a0ef004329447837329259105ad9cfc55c0e53c7
Akshay Chaudhary <akshaypoonia00000@gmail.com>
2025-07-12 23:56:44 +0530

    More problems solve

--- Leetcode problems/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
+++ Leetcode problems/34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.js
@@ -31,32 +31,61 @@
 // Solution time complexity O(log n)
 
 
-const array = [5,7,7,8,8,10];
-// const array = []
+// const array = [5,7,7,8,8,10];
+const array = []
 
 function searchRange(array, target) {
 
-    const result = [];
+    function findFirst(array, target) {
 
-    let start = 0;
-    let end = array.length-1;
+        let start = 0;
+        let end = array.length-1;
+        let first;
 
-    while(start < end) {
+        while(start <= end) {
+            let mid = Math.floor((start + end)/2);
 
-        let mid = Math.floor((start + end)/2);
-
-        for(let i=0; i<array.length; i++) {
-            if(target > array[mid]) {
+            if(target == array[mid]) {
+                first = mid;
+                end = mid - 1;
+            }
+            else if(target > array[mid]) {
                 start = mid + 1;
             }
-            else {
-                if(target < array[mid]) {
-                    end = mid - 1;
-                }
-                else if(target == array[mid]) {
-                    
-                }
-            }
+            else end = mid - 1;
         }
+
+        return first;
     }
+
+
+    function findLast(array, target) {
+        let start = 0;
+        let end = array.length-1;
+        let last;
+
+        while(start <= end) {
+
+            let mid = Math.floor((start + end)/2);
+
+            if(target == array[mid]) {
+                last = mid;
+                start = mid + 1;
+            }
+            else if(target < array[mid]) {
+                end = mid - 1;
+            }
+            else start = mid + 1;
+        }
+
+        return last;
+    }
+
+    let first = findFirst(array, target);
+    let last = findLast(array, target);
+
+    return first && last ? [first, last] : [-1, -1]
 }
+
+
+console.log(searchRange(array, 0));
